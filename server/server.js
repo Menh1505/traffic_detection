c46450cd08ed852a9901c3a00f223195cc71cb58
menh <dinhthienmenh1505@gmail.com>
2025-04-11 20:50:38 +0700

    modify: log port

--- server/server.js
+++ server/server.js
@@ -27,5 +27,5 @@ io.on("connection", (socket) => {
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port", process.env.PORT || 8080);
 });
