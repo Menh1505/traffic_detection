207a88043ea6cdc89ccc13d6c111d6882227c37a
menh <dinhthienmenh1505@gmail.com>
2025-04-11 22:41:51 +0700

    update: stream video with ffmpeg

--- mock-camera/server.js
+++ mock-camera/server.js
@@ -1,4 +1,5 @@
 const RtspServer = require("rtsp-streaming-server").default;
+const ffmpeg = require("fluent-ffmpeg");
 
 const server = new RtspServer({
   serverPort: 5554,
@@ -7,10 +8,27 @@ const server = new RtspServer({
   rtpPortEnd: 10050,
 });
 
+const videoPath = "./videos/test.mp4"; // your video path
+
 async function run() {
   try {
     await server.start();
     console.log("RTSP server started on rtsp://localhost:5554/live");
+
+    // Setting ffmpeg to stream the video to the RTSP server
+    ffmpeg(videoPath)
+      .inputOptions("-stream_loop -1") // loop video
+      .outputOptions("-f rtsp")
+      .output("rtsp://localhost:5554/live")
+      .on("start", function (commandLine) {
+        console.log("FFmpeg process started: " + commandLine);
+      })
+      .on("error", function (err) {
+        console.error("FFmpeg error: " + err.message);
+      })
+      .on("end", function () {
+        console.log("FFmpeg process finished.");
+      });
   } catch (e) {
     console.error(e);
   }
