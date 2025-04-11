c379a5ada32c26532fcec0a4f94e597b998a355f
menh <dinhthienmenh1505@gmail.com>
2025-04-11 22:32:08 +0700

    fix: move server to function

--- mock-camera/server.js
+++ mock-camera/server.js
@@ -1,28 +1,19 @@
-const RtspServer = require('rtsp-streaming-server').default;
-const fs = require('fs');
-const path = require('path');
+const RtspServer = require("rtsp-streaming-server").default;
 
 const server = new RtspServer({
-  serverPort: 554,
+  serverPort: 5554,
   clientPort: 6554,
   rtpPortStart: 10000,
   rtpPortEnd: 10050,
 });
 
-const videoPath = path.join(__dirname, './videos/test.mp4'); // Đường dẫn đến file video
-const streamName = 'live'; // Tên stream
+async function run() {
+  try {
+    await server.start();
+    console.log("RTSP server started on rtsp://localhost:5554/live");
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-server.on('clientConnected', client => {
-  console.log('Client connected');
-  client.on('teardown', () => console.log('Client disconnected'));
-});
-
-server.addStream({
-  name: streamName,
-  streamUrl: `file://${videoPath}`, // Đường dẫn đến file video
-});
-
-server.start();
-
-console.log(`RTSP server started on port ${server.options.serverPort}`);
-console.log(`Stream available at rtsp://localhost:${server.options.serverPort}/${streamName}`);
+run();
