68934669da1fb2824cd0d31cc5f037c736ed0be3
menh <dinhthienmenh1505@gmail.com>
2025-04-12 21:06:23 +0700

    feat: ffmpeg config

---
+++ server/src/configs/config.ffmpeg.js
@@ -0,0 +1,28 @@
+const ffmpegConfig = {
+  input: {
+    rtsp: "rtsp://localhost:5554/live",
+  },
+  output: {
+    hls: {
+      path: "./streams/output.m3u8",
+      options: [
+        "-c:v",
+        "copy", // Giữ nguyên codec video
+        "-c:a",
+        "aac", // Chuyển đổi audio sang AAC
+        "-hls_time",
+        "2", // Độ dài segment 2 giây
+        "-hls_list_size",
+        "5", // Giữ 5 segment trong playlist
+        "-hls_flags",
+        "delete_segments+append_list", // Tự động xóa segment cũ
+        "-hls_segment_type",
+        "fmp4", // Sử dụng định dạng fMP4
+        "-tune",
+        "zerolatency", // Tối ưu cho độ trễ thấp
+      ],
+    },
+  },
+};
+
+module.exports = ffmpegConfig;
