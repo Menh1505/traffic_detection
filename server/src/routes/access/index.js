3759430635c4667b4db4382c674c534e663e16ba
menh <dinhthienmenh1505@gmail.com>
2025-04-12 23:11:58 +0700

    feat: router get traffic status

--- server/src/routes/access/index.js
+++ server/src/routes/access/index.js
@@ -3,6 +3,13 @@
 const express = require("express");
 const router = express.Router();
 
-// get videos
+// get traffic status
+router.get("/traffic", (req, res) => {
+  const trafficStatus = {
+    status: "OK",
+    message: "Traffic is normal",
+  };
+  res.json(trafficStatus);
+});
 
 module.exports = router;
